30ee9f8038455a15270e5e11af6437daa479d1e7
Simon Lauridsen <you@example.com>
2022-02-07 12:17:12 +0100

    commit lavet filtrering

---
+++ burger.js
@@ -0,0 +1,16 @@
+const knapper = document.querySelector(".toggle_knap");
+const nav = document.querySelector(".burgermenuen");
+
+knapper.addEventListener("click", toggleMenu);
+
+function toggleMenu() {
+  nav.classList.toggle("shown");
+
+  const menuShown = nav.classList.contains("shown");
+
+  if (menuShown) {
+    knapper.classList.add("open");
+  } else {
+    knapper.classList.remove("open");
+  }
+}
